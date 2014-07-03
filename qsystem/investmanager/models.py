1ffbbc4fd596910f4f2696548ef765a86a11920b
Kevin_Xi <kevin.xgr@gmail.com>
2014-07-03 11:45:02 +0800

    Add and apply app investmanager

---
+++ qsystem/investmanager/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from accounts.models import User
+
+class Questionnaire(models.Model):
+	'''Questionnaire model
+
+	follow database design doc'''
+
+	title = models.CharField(max_length=255)
+	subject = models.CharField(max_length=255)
+	description = models.TextField(max_length=255)
+	content = models.PositiveIntegerField()
+	author = models.ForeignKey(User, related_name='Investigator')
+	date = models.DateTimeField()
