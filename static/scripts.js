5bca8d13f5c59077c1bfa2336c674882878b3473
Yugnaynehc <cyyyyc123@hotmail.com>
2014-07-04 17:14:00 +0800

    merge 4.html and 5.html to 4_5.html

--- static/scripts.js
+++ static/scripts.js
@@ -1,4 +1,4 @@
-function checkValid(){
+﻿function checkValid(){
     var questElement = document.quest.elements;
     for(var i=0; i<questElement.length-1;i++){
 	if(trim(questElement[i].value)==""){
@@ -18,44 +18,101 @@ function trim(str){
 }
 
 
-function add_question()
+function addQuestion()
 {
     var parent = document.getElementById("upload");
-    
+	// 如何维护一个全局的no?
+    var no = document.getElementsByClassName("question").length;
+	
     var question = document.createElement("div");
     question.setAttribute("class", "question");
-
+	
     var select = document.getElementById('select');
-    var p = document.createElement("p");
     var index = select.selectedIndex;
-    var text = select.options[index].text;
-    p.textContent = text;
-    p.id = text;
-
+	var selected = select.options[index];
+	var p = document.createElement("p");
+    p.textContent = (no + 1) + '.' + selected.text;
+	question.id = selected.value;
+	
     var delQue = document.createElement("button");
     delQue.setAttribute("class", "delItem");
-    delQue.textContent = "delete Question";
+	delQue.setAttribute("onclick", "deleteQuestion(this)");
+    delQue.textContent = "删除本题";
     
-    var inp = document.createElement("input");
-    inp.setAttribute("type", "text");
+    var describe = document.createElement("input");
+    describe.setAttribute("type", "text");
 
-    var inc = document.createElement("button");
+    var inc = document.createElement("input");
+	inc.setAttribute("type", "button");
     inc.setAttribute("class", "addItem");
-    inc.textContent = "add Item";
+	inc.setAttribute("onclick", "addItem(this)");
+    inc.setAttribute("value", "增加选项");
 
-    var del = document.createElement("button");
+    var del = document.createElement("input");
+	del.setAttribute("type", "button");
     del.setAttribute("class", "delItem");
-    del.textContent = "delete Item";
+	del.setAttribute("onclick", "deleteItem(this)");
+    del.setAttribute("value", "删除选项");
 
     question.appendChild(p);
+	question.appendChild(describe);
     question.appendChild(delQue);
-    question.appendChild(inp);
     question.appendChild(inc);
     question.appendChild(del);
-
-    parent.appendChild(question);
+	appendNewLine(question);
+	
+	var submit = document.getElementById("addQuestion");
+	
+    //parent.appendChild(question);
+	parent.insertBefore(question, submit);
 }
 
-function delete_question() {
-    
+function deleteQuestion(obj) {
+    var parent = document.getElementById("upload");
+	parent.removeChild(obj.parentNode);
+}
+
+function addItem(obj) {
+	var parent = obj.parentNode;
+	var no = parent.getElementsByClassName("item").length;
+	var type = parent.id;
+	
+	var item = document.createElement("div");
+	var choose = document.createElement("input");
+	var label = document.createElement("label");
+	var text = document.createElement("input");
+	item.setAttribute("class", "item");
+	choose.setAttribute("type", "checkbox");
+	choose.setAttribute("class", "checkbox");
+	label.textContent = (no + 1) + '.';
+	text.setAttribute("type", "text");
+	item.appendChild(choose);
+	item.appendChild(label);
+	item.appendChild(text);
+	parent.appendChild(item);
+}
+
+function deleteItem(obj) {
+	var parent = obj.parentNode;
+	var items = parent.getElementsByClassName("item");
+	//alert(""+items.length);
+	for (var i=items.length-1; i>=0; --i) {
+		var checkBoxs = items[i].getElementsByClassName("checkbox");
+		if (checkBoxs[0].checked) {
+			items[i].remove();
+		}
+	}
+	items = parent.getElementsByClassName("item");
+	for (var i=0; i<items.length; ++i) {
+		var label = items[i].getElementsByTagName("label")[0];
+		label.textContent = (i + 1) + '.';
+	}	
+}
+
+function appendNewLine(obj) {
+	obj.appendChild(document.createElement("br"));
+}
+
+function Valid() {
+	return true;
 }
