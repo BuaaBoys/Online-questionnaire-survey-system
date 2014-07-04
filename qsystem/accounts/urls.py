32f8aeab77b0e92bc1c0c4e9a71f34b7c28648aa
mgnhjkl <mgnhjkl@163.com>
2014-07-04 11:07:52 +0800

    init login views

--- qsystem/accounts/urls.py
+++ qsystem/accounts/urls.py
@@ -4,9 +4,6 @@ from accounts import views
 urlpatterns = patterns('',
 	url(r'^register/$', views.register, name = 'register'),
 	url(r'^register/submit/$', views.register_submit, name = 'register_submit'),
-<<<<<<< HEAD
 	url(r'^login/$', vies.login, name = 'login'),
 	url(r'^login/submit/$', vies.login_submit, name = 'login'),
-=======
->>>>>>> Deploy static dir
 )		
