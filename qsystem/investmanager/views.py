7169bce17ed43ea4202127babfa449684b328fb4
Kevin_Xi <kevin.xgr@gmail.com>
2014-07-04 11:38:15 +0800

    Complete validations

--- qsystem/investmanager/views.py
+++ qsystem/investmanager/views.py
@@ -13,7 +13,10 @@ from models import Questionnaire
 from form import QuestForm
 
 def show_quest_fill_page(request):
-	return render(request, "investmanager/4.html", {})
+	'''let investigator create the questionnaire'''
+
+	return render_to_response("investmanager/4.html", {})
+
 
 def publish(request):
 	'''pass basic infomation to next page
@@ -21,6 +24,25 @@ def publish(request):
 	when the button is pressed, the arguments will be passed.'''
 
 	form = QuestForm(request.POST)
-	form.save()
-	# this place manage the content to xml conversion, use the id which database automatic generate
-	return HttpResponseRedirect("/")
+	if form.is_valid():
+		quest = form.save()
+
+		# this place manage the content to xml conversion, use the id which database automatic generate
+		return HttpResponseRedirect(str(quest.id))
+
+
+def quest(request, no):
+	'''let people fill the questionnaire'''
+
+	try:
+		no = int(no)
+		quest = Questionnaire.objects.get(id=no)
+	except:
+		raise Http404()
+
+	id = quest.id
+	title = quest.title
+	subject = quest.subject
+	description = quest.description
+
+	return render_to_response("investmanager/show_quest.html",{'id':id, "title":title, "subject":subject, "description":description,})
