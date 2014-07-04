124cdc1a558654d7ad10928e439371e5a2b755fd
mgnhjkl <mgnhjkl@163.com>
2014-07-04 11:34:38 +0800

    init login

---
+++ qsystem/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+from accounts import views
+
+urlpatterns = patterns('',
+	url(r'^register/$', views.register, name = 'register'),
+	url(r'^register/submit/$', views.register_submit, name = 'register_submit'),
+	url(r'^login/$', vies.login, name = 'login'),
+	url(r'^login/submit/$', vies.login_submit, name = 'login'),
+)		
---
+++ qsystem/accounts/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from accounts.models import User, UserForm
+from datetime import datetime
+from django.core.exceptions import ObjectDoesNotExist
+
+def register(request):
+	is_error = "hidden"
+	error_msg = ""
+	return render(request, 'accounts/register.html', {"error_msg":error_msg, "is_error":is_error})
+
+def register_submit(request):
+	try:
+		User.objects.get(email=request.POST['email'])
+	except:
+		form = UserForm(request.POST)
+		form.save()
+		is_error = "hidden"
+		error_msg = ""
+		return render(request, 'accounts/register.html', {"error_msg":error_msg, "is_error":is_error})
+	is_error = ""
+	error_msg = "Existed E-mail Address"
+	return render(request, 'accounts/register.html', {"error_msg":error_msg, "is_error":is_error})
+
+def login(request):
+	is_error = False
+	error_msg = ""
+	return(request, "accounts/login.html",{"error_msg":error_msg, "is_error":is_error})
+
+def login_submit(request):
+	try:
+		user = User.objects.get(email=request.POST['email'])
+	except:
+		is_error = True
+		error_msg = "E-mail does not exist"
+		return render(request, "accounts/login.html", {"error_msg":error_msg, "is_error":is_error})
