ea0a9cdb264b27541699757161dfb984f41261af
Yugnaynehc <cyyyyc123@hotmail.com>
2014-07-05 03:33:42 +0800

    beauty views and fix static files bug

--- qsystem/investmanager/views.py
+++ qsystem/investmanager/views.py
@@ -16,8 +16,8 @@ from questions import *
 def show_quest_fill_page(request):
 	'''let investigator create the questionnaire'''
 
-	return render_to_response("investmanager/4_5.html", {})
-
+	#return render_to_response("investmanager/4_5.html", {})
+	return render(request, "investmanager/4_5.html", {})
 
 def publish(request):
 	'''pass basic infomation to next page
@@ -26,11 +26,7 @@ def publish(request):
 
 	form = QuestForm(request.POST)
 	if form.is_valid():
-<<<<<<< HEAD
 		quest = form.save(request)
-
-=======
-		quest = form.save()
                 try:
                         questions = Questions(qid=str(quest.id), qs=[])
                         questionTitles = request.POST.getlist('question')
@@ -49,7 +45,6 @@ def publish(request):
                         questions.write()
                 except Exception, e:
                         print e
->>>>>>> get questions|saved as .xml file
 		# this place manage the content to xml conversion, use the id which database automatic generate
                 return HttpResponseRedirect(str(quest.id))
 
@@ -68,4 +63,4 @@ def quest(request, no):
 	subject = quest.subject
 	description = quest.description
 
-	return render_to_response("investmanager/show_quest.html",{'id':id, "title":title, "subject":subject, "description":description,})
+	return render(request, "investmanager/show_quest.html",{'id':id, "title":title, "subject":subject, "description":description,})
