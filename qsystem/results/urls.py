15c3c703dc805c872618040a96ee065cbe65faa2
CoderSherlock <haopengzhan@gmail.com>
2014-07-07 15:55:07 +0800

    Data Flew Finished
    Best regards to Mengshen

--- qsystem/results/urls.py
+++ qsystem/results/urls.py
@@ -5,6 +5,5 @@ from results import views
 urlpatterns = patterns('',
 	url(r'^(?P<qid>\d+)$',views.answer),
 	url(r'^(?P<qid>\d+)_$',views.publish),
-	url(r'^success.html$',views.success)
-	#url(r'^error404.html$',views.error404)
+	url(r'^(?P<qid>\d+)/results$',views.result)
 )
--- qsystem/results/views.py
+++ qsystem/results/views.py
@@ -5,6 +5,7 @@ from results.models import Result
 from investmanager.models import Questionnaire 
 import xml.dom.minidom
 from results.questions.questions import Questions
+from qsystem.views import AlertMessage
 
 
 def answer(request, qid):
@@ -17,7 +18,7 @@ def answer(request, qid):
 		q = get_object_or_404(Questionnaire, pk=qid)
 		return render(request, 'results/answer.html' ,{'Questionnaire':q ,'naire':Naire ,'qid':qid})
 	except:
-		return render(request, 'results/error404.html')
+		return render(request, "homepage/message.html", {"message": AlertMessage("danger", "Page 404!", "xxxx", "/"),})
 
 def publish(request, qid):
 	try:
@@ -25,31 +26,32 @@ def publish(request, qid):
 		Naire.clean()
 		Naire.qid = qid
 		Naire.read()
-		result = ''
+		result = []
 		for x in xrange(1,Naire.count+1):
 			if Naire.questionList[x-1].qtype == 'single':
-				result += request.POST[str(x)] + ','
+				result.append([str(request.POST[str(x)])])
 			elif Naire.questionList[x-1].qtype == 'multiply':
 				rlist = request.REQUEST.getlist(str(x))
 				if len(rlist)==0:
 					raise Exception()
-				m = '['
+				m = []
 				for r in rlist:
-					m += r + ','
-				m += '],'
-				result += m
+					m .append(r) 
+				result.append(m)
 			elif Naire.questionList[x-1].qtype == 'judge':
-				result += request.POST[str(x)] + ','
+				result.append([str(request.POST[str(x)])])
 			elif Naire.questionList[x-1].qtype == 'essay':
-				result += request.POST[str(x)] + ','
+				if str(request.POST[str(x)]) !=  '' or str(request.POST[str(x)]) !=' ':
+					result.append([str(request.POST[str(x)])])
 
 		user = "anonymity@admin.com"
 		user = request.COOKIES.get("email")
 		if user == None:
 			user = "anonymity@admin.com"
-		r = Result(questionnaire_id=qid,participant_id=user,answer=result)
+		r = Result(questionnaire_id=qid,participant_id=user,answer=str(result))
+		print str(result)
 		r.save()
-		return render(request, 'results/success.html')
+		return render(request, "homepage/message.html", {"message": AlertMessage("success", "Success!", "You are logged out now.", "/"),})
 	except:
 		q = get_object_or_404(Questionnaire, pk=qid)
 		Naire = Questions()
@@ -59,14 +61,18 @@ def publish(request, qid):
 		return render(request, 'results/answer.html',{'Questionnaire':q ,'naire':Naire ,'qid':qid, 'errorMsg':'Not finished yet!'})	
 		
 def success(request):
-	return render(request, 'success.html')
+	return render(request, "homepage/message.html", {"message": AlertMessage("success", "Success!", "You are logged out now.", "/"),})
 
 def error404(request):
-	try:
-		Naire = Questions()
-		Naire.qid = qid 
-		Naire.read()
-		q = get_object_or_404(Questionnaire, pk=qid)
-		return render(request, 'results/answer.html' ,{'Questionnaire':q ,'naire':Naire ,'qid':qid})
-	except:
-		return render(request, 'results/error404.html')
+	return render(request, "homepage/message.html", {"message": AlertMessage("danger", "Page 404!", "xxxx", "/"),})
+
+def result(request, qid):
+	Naire = Questions()
+	Naire.clean()
+	Naire.qid = qid
+	Naire.read()
+	q = get_object_or_404(Questionnaire, pk=qid)
+	rt = get_object_or_404(Result, questionnaire_id=qid, participant_id="anonymity@admin.com")
+	r = eval(rt.answer)
+	print r
+	return render(request, 'results/results.html' ,{'Questionnaire':q ,'naire':Naire ,'qid':qid ,'result':r})
